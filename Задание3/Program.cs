5998b0f2342161526e7f05deae1911746ac8a6b3
Petryakov_Sergey <petrykov2501@gmail.com>
2021-12-25 00:36:17 +0500

    task 3

---
+++ Задание3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N ");
+int x = int.Parse(Console.ReadLine());
+int i = 0;
+while (i <= x * 2)
+{
+    int counts = -x + i;
+    Console.Write(counts + " ");   
+    i ++;
+}
