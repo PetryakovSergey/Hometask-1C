436077b1d5f849f8e12f9aba5572f15710f82622
Petryakov_Sergey <petrykov2501@gmail.com>
2021-12-25 00:49:14 +0500

    task4

---
+++ Задание4/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число : ");
+int number = int.Parse(Console.ReadLine());
+int third_number = number % 10; 
+Console.WriteLine("Последняя цифра "+third_number);
