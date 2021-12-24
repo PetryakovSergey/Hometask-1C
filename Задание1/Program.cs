7701ba5b58a762426d6a631e1c70935e2f72964e
Petryakov_Sergey <petrykov2501@gmail.com>
2021-12-25 00:34:36 +0500

    task 1

---
+++ Задание1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число  ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число  ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("число А является квадратом числа В");
+}
+else
+{
+    Console.WriteLine("число А не является квадратом числа В");
+}
